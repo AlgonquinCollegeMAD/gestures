9cf32c561bc4de818a66f061abcf6e8784f3d8bd
Vladimir Cezar <vladimircezar@icloud.com>
2023-10-17 16:32:07 -0400

    Add circles to screen

--- Gestures/ContentView.swift
+++ Gestures/ContentView.swift
@@ -1,24 +1,58 @@
-//
-//  ContentView.swift
-//  Gestures
-//
-//  Created by Vladimir Cezar on 2023-10-17.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+struct TheThing: Identifiable {
+  var id: String
+  var circle: Circle
+  var size: CGFloat
+  var position: CGPoint
+  var color = Color(
+    red: Double.random(in: 0...1),
+    green: Double.random(in: 0...1),
+    blue: Double.random(in: 0...1)
+  )
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+  @State private var thingsOnScreen: [TheThing] = []
+  
+  var body: some View {
+    NavigationStack {
+      ZStack {
+        ForEach(thingsOnScreen) { index in
+          index.circle
+            .foregroundColor(index.color)
+            .position(index.position)
+            .frame(width: index.size, height: index.size)
+        }
+      }
+      .navigationTitle("Circles")
+      .toolbar{
+        ToolbarItemGroup(placement: .topBarTrailing) {
+          Button {
+            addCircle()
+          } label: {
+            Image(systemName: "plus.circle")
+          }
+        }
+      }
+    }
+  }
+  
+  func addCircle() {
+    let theThing = TheThing(
+      id: UUID().uuidString,
+      circle: Circle(),
+      size: CGFloat.random(in: 50...150),
+      position: randomPoint()
+    )
+    
+    thingsOnScreen.append(theThing)
+  }
+  
+  func randomPoint() -> CGPoint {
+    let x = CGFloat.random(in: 0...300) // Adjust the range as needed
+    let y = CGFloat.random(in: 0...300) // Adjust the range as needed
+    return CGPoint(x: x, y: y)
+  }
+  
 }
