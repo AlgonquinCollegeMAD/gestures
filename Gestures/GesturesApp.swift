a530d6ffb28e625e79bc5976b2557820fe89c9cf
“Vladimir <vladimircezar@icloud.com>
2023-10-17 14:21:26 -0400

    Initial Commit

---
+++ Gestures/GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesApp.swift
+//  Gestures
+//
+//  Created by Vladimir Cezar on 2023-10-17.
+//
+
+import SwiftUI
+
+@main
+struct GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
