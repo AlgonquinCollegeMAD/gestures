1b02921f830168f3cc62ae3b39752ec0d080f403
Vladimir Cezar <vladimircezar@icloud.com>
2023-10-19 18:15:00 -0400

    Refactor draw and erase functions

--- Gestures/Thing.swift
+++ Gestures/Thing.swift
@@ -1,8 +1,18 @@
 import SwiftUI
 
-enum Shape {
-    case circle(Circle = Circle())
-    case roundedRect(RoundedRectangle = RoundedRectangle(cornerSize: CGSize(width: 20, height: 20)))
+enum Shape: Equatable {
+  static func == (lhs: Shape, rhs: Shape) -> Bool {
+    switch (lhs, rhs) {
+    case (.circle(_), .circle(_)):
+      return true
+    case (.roundedRect(_), .roundedRect(_)):
+      return true
+    default:
+      return false
+    }
+  }
+  case circle(Circle = Circle())
+  case roundedRect(RoundedRectangle = RoundedRectangle(cornerSize: CGSize(width: 20, height: 20)))
 }
 
 struct Thing: Identifiable, Equatable {
